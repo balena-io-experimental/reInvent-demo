8f71d0438dce8a4f53464a51a4f1a2cf0c7131b7
gaudi <craig@resin.io>
2016-05-10 17:45:13 +0100

    test

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ else
   if [[ -z "$AWS_CERT" && -z "$AWS_PRIVATE_KEY" && -z "$AWS_ROOT_CA" ]]
   then
     echo "Creating AWS certificates"
+    curl https://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem > /data/rootCA.pem
     curl -X POST -H "Cache-Control: no-cache" -d '{ "uuid": "'$RESIN_DEVICE_UUID'", "attributes": { "someKey": "someVal" } }' $LAMBDA
   elif [[ "$AWS_CERT" && "$AWS_PRIVATE_KEY" && "$AWS_ROOT_CA" ]]
     then
