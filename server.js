15c174ecbadfe246c827ffab4edfaddf7b1614fe
gaudi <craig@resin.io>
2016-05-10 16:35:55 +0100

    test

--- server.js
+++ server.js
@@ -4,26 +4,46 @@ var Chance = require('chance'); // used to randomize bool values
     chance = new Chance();
 
 var pattern = /#####/g;
+var fs = require('fs');
+//
+// var device = awsIot.device({
+// privateKey: new Buffer(process.env.AWS_PRIVATE_KEY.replace(pattern, '\n')),
+// clientCert: new Buffer(process.env.AWS_CERT.replace(pattern, '\n')),
+//     caCert: new Buffer(eol.crlf(process.env.AWS_ROOT_CA.replace(pattern, '\n'))),
+//   clientId: process.env.RESIN_DEVICE_UUID,
+//     region: process.env.AWS_REGION
+// });
+//
+// device.on('connect', function() {
+//   console.log('connect');
+//   device.subscribe('topic_1');
+//
+//   // publish data every second
+//   setInterval(function () {
+//     var bool = chance.bool({likelihood: 10});
+//     device.publish('topic_1', JSON.stringify({ test_data: bool }));
+//   }, 5000);
+// });
+//
+// device.on('message', function(topic, payload) {
+//   console.log('message', topic, payload.toString());
+// });
 
-var device = awsIot.device({
-privateKey: new Buffer(process.env.AWS_PRIVATE_KEY.replace(pattern, '\n')),
-clientCert: new Buffer(process.env.AWS_CERT.replace(pattern, '\n')),
-    caCert: new Buffer(eol.crlf(process.env.AWS_ROOT_CA.replace(pattern, '\n'))),
-  clientId: process.env.RESIN_DEVICE_UUID,
-    region: process.env.AWS_REGION
+fs.readFile('/data/rootCA.pem', 'utf8', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    // console.log(new Buffer());
+    run(data)
 });
 
-device.on('connect', function() {
-  console.log('connect');
-  device.subscribe('topic_1');
-
-  // publish data every second
-  setInterval(function () {
-    var bool = chance.bool({likelihood: 10});
-    device.publish('topic_1', JSON.stringify({ test_data: bool }));
-  }, 5000);
-});
-
-device.on('message', function(topic, payload) {
-  console.log('message', topic, payload.toString());
-});
+function run(data) {
+  //
+  console.log(data);
+  console.log(process.env.AWS_ROOT_CA);
+  var pattern = /#####/g;
+  console.dir(bufferEqual(
+      new Buffer(data),
+      new Buffer((process.env.AWS_ROOT_CA.replace(pattern, '\r\n'))
+  ));
+}
