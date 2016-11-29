6d8f2191202c760c06911e33e94a8b1d944b622c
gaudi <craig@resin.io>
2016-11-29 14:09:47 -0800

    test

--- lcd.js
+++ lcd.js
@@ -1,13 +1,11 @@
 const pitft = require("pitft")
 const chroma = require("chroma-js")
 
-const fb = pitft("/dev/fb1") // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
-
-// Clear the screen buffer
+// Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
+const fb = pitft("/dev/fb1")
 fb.clear();
-var xMax = fb.size().width;
-var yMax = fb.size().height;
-
+const xMax = fb.size().width;
+const yMax = fb.size().height;
 const scale = chroma.scale(['green', 'red']).domain([ 0, -80 ]);
 
 module.exports = {
@@ -18,7 +16,7 @@ module.exports = {
     fb.color(rgb[0], rgb[1], rgb[2]);
     fb.rect(0, 0, xMax, yMax, true); // Draw a filled rectangle
     fb.color(0, 0, 0);
-    fb.text(xMax/2, yMax/2-24, network.ssid, true, 0);
+    fb.text(xMax/2, yMax/2-26, network.ssid, true, 0);
     fb.text(xMax/2, yMax/2, network.signal, true, 0);
   }
 }
